0f6de160359b3a997b5d042a692f71bbffd30bce
Nicholas Albion <nalbion@yahoo.com>
2016-02-04 02:36:44 +1100

    Array<any>

--- i18next/i18next.d.ts
+++ i18next/i18next.d.ts
@@ -121,7 +121,7 @@ interface I18nextStatic {
         load: (languages: string[], options: I18nextOptions, callback: (err: Error, store: IResourceStore) => void ) => void;
         postMissing: (language: string, namespace: string, key: string, defaultValue: any, languages: string[]) => void;
     };
-    t(key: string, options?: I18nTranslateOptions): string|Object|Array;
+    t(key: string, options?: I18nTranslateOptions): string|Object|Array<any>;
     translate(key: string, options?: I18nTranslateOptions): string;
     exists(key: string, options?: any): boolean;
 }
